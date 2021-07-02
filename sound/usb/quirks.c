17e68ac9525cfc6fc72965e5461fe266b0955ee4
Spockfish <htenberge@gmail.com>
2021-07-02 17:56:59 +0200

    We've added the Auralic Altair G2.1 to the quirks, but for altset 2. And... it should be altset 3. This patch fixes that.

--- sound/usb/quirks.c
+++ sound/usb/quirks.c
@@ -1751,7 +1751,6 @@ u64 snd_usb_interface_dsd_format_quirks(struct snd_usb_audio *chip,
 	case USB_ID(0x139f, 0x5504): /* Nagra DAC */
 	case USB_ID(0x20b1, 0x3008): /* iFi Audio micro/nano iDSD */
 	case USB_ID(0x1511, 0x0037): /* AURALiC VEGA */
-	case USB_ID(0x1511, 0x0064): /* AURALiC ALTAIR G2.1 */
 	case USB_ID(0x20b1, 0x0002): /* Wyred 4 Sound DAC-2 DSD */
 	case USB_ID(0x20b1, 0x2004): /* Matrix Audio X-SPDIF 2 */
 	case USB_ID(0x20b1, 0x2008): /* Matrix Audio X-Sabre */
@@ -1770,6 +1769,7 @@ u64 snd_usb_interface_dsd_format_quirks(struct snd_usb_audio *chip,
 
 	case USB_ID(0x0d8c, 0x0316): /* Hegel HD12 DSD */
 	case USB_ID(0x10cb, 0x0103): /* The Bit Opus #3; with fp->dsd_raw */
+	case USB_ID(0x1511, 0x0064): /* AURALiC ALTAIR G2.1 */
 	case USB_ID(0x152a, 0x85dd): /* SMSL M300 */
 	case USB_ID(0x152a, 0x85de): /* SMSL D1 DAC */
 	case USB_ID(0x152a, 0x8750): /* Topping DX7s */
