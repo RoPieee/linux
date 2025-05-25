1ed070b28ab4383afd8a1dd637db953d01f7f5f3
Spockfish <htenberge@gmail.com>
2025-05-25 10:27:08 +0200

    fix compile error

--- drivers/net/wireless/realtek/rtw88/mac80211.c
+++ drivers/net/wireless/realtek/rtw88/mac80211.c
@@ -928,10 +928,9 @@ static int rtw_ops_set_sar_specs(struct ieee80211_hw *hw,
 
 static void rtw_ops_sta_rc_update(struct ieee80211_hw *hw,
 				  struct ieee80211_vif *vif,
-				  struct ieee80211_link_sta *link_sta,
+				  struct ieee80211_sta *sta,
 				  u32 changed)
 {
-	struct ieee80211_sta *sta = link_sta->sta;
 	struct rtw_dev *rtwdev = hw->priv;
 	struct rtw_sta_info *si = (struct rtw_sta_info *)sta->drv_priv;
 
